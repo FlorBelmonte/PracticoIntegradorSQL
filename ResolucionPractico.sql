9c896cd485777974ca24e111daebcffdca21a932
florbelmonte <mariaflorenciabelmonterojas@gmail.com>
2023-07-03 19:12:39 -0300

    git commit -m "actualizacion del practico"

--- ResolucionPractico.sql
+++ ResolucionPractico.sql
@@ -50,4 +50,55 @@ FROM Cliente
 JOIN Compra ON Cliente.idCliente = Compra.idCliente;
 
 
+-- Crea una VIEW que muestre los productos con descuento.
 
+CREATE VIEW ProductosConDescuento AS
+SELECT * FROM Producto WHERE descuento > 0;
+
+select * from Productoscondescuento;
+
+-- Crea un INDEX en la columna "nombre" de la tabla "Productos" para mejorar la velocidad de las consultas.
+
+CREATE INDEX idx_nombre ON Producto (nombre);
+
+-- 5) Procedimientos almacenados:
+-- ● Crea un STORE PROCEDURE que calcule el total de ventas para un cliente dado.
+
+DELIMITER //
+
+CREATE PROCEDURE calcular_total_ventas(IN cliente_id INT, OUT total_ventas DECIMAL(10, 2))
+BEGIN
+    SELECT SUM(p.precio * c.cantidad) INTO total_ventas
+    FROM Compra c
+    JOIN Producto p ON c.idProducto = p.idProducto
+    WHERE c.idCliente = cliente_id;
+END //
+
+DELIMITER ;
+
+-- Utiliza el STORE PROCEDURE para obtener el total de ventas de un cliente específico.
+
+CALL calcular_total_ventas(12, @total_ventas_cliente); --ejemplo con Cliente Id 12
+
+SELECT @total_ventas_cliente;
+
+--6. Funciones:
+--● Crea una función que calcule el promedio de precios de los productos.
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `CalcularPromedioDePrecios`() RETURNS decimal(10,0)
+    READS SQL DATA
+BEGIN
+  DECLARE promedio DECIMAL;
+  SELECT AVG(precio) INTO promedio
+  FROM Producto;
+  RETURN promedio;
+END
+
+--● Utiliza la función para obtener el promedio de precios de todos los productos.
+select practicointegrador.CalcularPromedioDePrecios();
+
+select CalcularPromedioDePrecios() as PromedioDePrecios;
+
+--7. Transacciones:
+--● Crea una transacción que inserte un nuevo cliente y una nueva orden de compra al mismo tiempo.
+--● Asegúrate de que la transacción se ejecute correctamente y se haga un rollback en caso de error.
